35420c36a3f92a71389265e6d1601083287383d1
SignedI <106474711+SignedI@users.noreply.github.com>
2023-07-13 16:52:09 +0530

    Add files via upload

---
+++ CNN implementation for MNIST.py
@@ -0,0 +1,39 @@
+import tensorflow as tf
+from tensorflow import keras
+from keras.datasets import mnist
+from keras.layers import Conv2D, Flatten
+import numpy as np
+import numpy.random as nr
+import matplotlib.pyplot as plt
+from keras.models import Sequential
+from keras.layers import Dense
+from sklearn.metrics import accuracy_score
+nc = 10 
+
+(Xtrain, ytrain), (Xtest, ytest) = mnist.load_data()
+
+#Show sample image
+plt.figure(1)
+imgplot1 = plt.imshow(Xtrain[nr.randint(60000)])
+plt.show()
+
+Xtrain = Xtrain.reshape(60000,28,28,1)
+Xtest = Xtest.reshape(10000,28,28,1)
+
+ytrainEnc = tf.one_hot(ytrain, depth=nc)
+ytestEnc = tf.one_hot(ytest, depth=nc)
+
+model = Sequential()
+model.add(Conv2D(64, kernel_size=3, activation="relu", input_shape=(28,28,1)))
+model.add(Conv2D(32, kernel_size=3, activation="relu"))
+model.add(Flatten())
+model.add(Dense(10, activation="softmax"))
+model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
+
+model.fit(Xtrain, ytrainEnc, validation_data=(Xtest, ytestEnc), epochs=3)
+
+ypred = model.predict(Xtest)
+ypred = np.argmax(ypred,axis=1)
+
+score = accuracy_score(ypred,ytest)
+print('Accuracy score is',100*score,'%')
